3db78aa3071f8b7a3e535cfa8339b1f1924d7536
Ivo Nascimento <iannsp@gmail.com>
2011-08-02 13:21:35 -0300

    change the credits

--- tests/colors.php
+++ tests/colors.php
@@ -7,7 +7,6 @@ require_once '../src/Console/Color2/Main.php';
 
 echo PEAR2\Console\Color2\Main::convert("%7%YHello %YWor%rld!%n\n");
 
-
 error_reporting(E_ALL);
 // Let's add a little color to the world
 // %n resets the color so the following stuff doesn't get messed up
@@ -20,7 +19,6 @@ print PEAR2\Console\Color2\Main::convert("%BHello World!%n\n");
 print PEAR2\Console\Color2\Main::convert("3 out of 4 people make up about %r75%% %nof the " 
                             ."world population.\n");
 // Or you can use the escape() method.
-
 print PEAR2\Console\Color2\Main::convert("%y"
      .PEAR2\Console\Color2\Main::escape('%4If you feel that you do everying wrong, be random'
                            .', there\'s a 50% Chance of making the right '
